5390523e84f2181b7dc58d67c9fcf512090ea990
ouyang99 <2215327408@qq.com>
2020-08-27 12:25:31 +0800

    测试

---
+++ demo.py
@@ -0,0 +1,23 @@
+
+# print("hello world",end="111")
+# print(2333)
+#  print(2.333)
+# print(True,False)
+# print(None)
+# print({})
+# print([])
+
+
+# # print("123","呼呼","hahha")
+
+# name = "李四"
+# print(name)
+# res = (3*4/6+7)%4
+# print(res)
+print("hahahaha","那呢")
+# a = input("请输入: ")
+# print("这是我输入的值: ",a)
+
+a = float(input("这是输入的第一个数: "))
+b = float(input("这是我输入的第二个数："))
+print("他们的和是: ",a+b)
